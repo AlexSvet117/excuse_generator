652fb58d860ff232cb00dec90fed58a7831f93b9
AlexSvet117 <107494487+AlexSvet117@users.noreply.github.com>
2024-11-29 19:57:39 +0000

    Inital css and JS

--- src/js/script.js
+++ src/js/script.js
@@ -7,9 +7,25 @@ window.onload = function () {
     const excuse = document.getElementById('excuse');
 
     // 2. Generate a random excuse
+    const quote1 = [
+        'My cat', 'My dog', 'My hamster', 'My neighbour', 'My pet alligator']
 
-    const excuseText = `The dog ate my homework for breakfast`;
+    const quote2 = ['ate my', 'stole my', 'hid my', 'destroyed my', 'traded my']
 
+    const quote3 = ['laptop', 'computer', 'car keys', 'wallet', 'house keys']
+
+    const quote4 = ['yesterday,', 'two minutes ago,', 'a year ago,', 'last winter,', 'this night,']
+
+    const quote5 = ['for real!', 'for real life!', 'I swear!', `it's true!`, 'I mean it!']
+
+    const random1 = Math.floor(Math.random() * quote1.length)
+    const random2 = Math.floor(Math.random() * quote2.length)
+    const random3 = Math.floor(Math.random() * quote3.length)
+    const random4 = Math.floor(Math.random() * quote4.length)
+    const random5 = Math.floor(Math.random() * quote5.length)
+
+    const fullQuote = `${quote1[random1]} ${quote2[random2]} ${quote3[random3]} ${quote4[random4]} ${quote5[random5]}`
+    
     // 3. Inseart the excuse into the excuse element
-    excuse.innerHTML = excuseText;
+    excuse.innerHTML = fullQuote;
 }
