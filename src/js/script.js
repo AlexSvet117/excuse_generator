d3ee7840be31a8d75d6dcc467b619544a062b19d
AlexSvet117 <107494487+AlexSvet117@users.noreply.github.com>
2024-12-01 19:55:55 +0000

    Changes to HTML and CSS

--- src/js/script.js
+++ src/js/script.js
@@ -5,18 +5,16 @@ window.onload = function () {
 
     // 1. Get the excuse element
     const excuse = document.getElementById('excuse');
+    const transcript = document.getElementById('transcript')
 
     // 2. Generate a random excuse
-    const quote1 = [
-        'My cat', 'My dog', 'My hamster', 'My neighbour', 'My pet alligator']
-
+    const quote1 = ['My cat', 'My dog', 'My hamster', 'My neighbour', 'My alligator']
     const quote2 = ['ate my', 'stole my', 'hid my', 'destroyed my', 'traded my']
-
     const quote3 = ['laptop', 'computer', 'car keys', 'wallet', 'house keys']
+    const quote4 = ['yesterday,', 'a minute ago,', 'a year ago,', 'last winter,', 'this night,']
+    const quote5 = ['he told me!', 'for real life!', 'I swear!', `it's true!`, 'I mean it!']
 
-    const quote4 = ['yesterday,', 'two minutes ago,', 'a year ago,', 'last winter,', 'this night,']
-
-    const quote5 = ['for real!', 'for real life!', 'I swear!', `it's true!`, 'I mean it!']
+    let transcriptMsg = ['Who to blame', 'What they did', 'What object suffered', 'When did it happen', 'Certainty lvl']
 
     const random1 = Math.floor(Math.random() * quote1.length)
     const random2 = Math.floor(Math.random() * quote2.length)
@@ -26,6 +24,13 @@ window.onload = function () {
 
     const fullQuote = `${quote1[random1]} ${quote2[random2]} ${quote3[random3]} ${quote4[random4]} ${quote5[random5]}`
     
-    // 3. Inseart the excuse into the excuse element
+    // 3. Insert the excuse into the excuse element
     excuse.innerHTML = fullQuote;
+    transcript.innerHTML = transcriptMsg;
 }
+
+// who to blame
+// what they did
+// what object suffered
+// when did it happen
+// certainty level
